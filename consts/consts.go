53949745d018532d9a3cb033d0f86d0b3e047111
Sruttz <110161415+Sruttz@users.noreply.github.com>
2023-09-01 19:37:24 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Srutzz"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Sruthika"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SRU"
 )
 
 var ID ids.ID
